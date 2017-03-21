63decd753a1fa360cc455502223ad8fd93606125
erodthup <e.rodthup@group-miki.com>
2017-03-21 09:15:25 +0000

    ignore real search

--- service-worker.js
+++ service-worker.js
@@ -69,7 +69,7 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  var dataUrl = 'https://test.mikinet.co.uk/interfaceWL/7.0/hotelSearch';
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
